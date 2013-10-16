1c13423a501a557f4563c774c9c3f51c266fade5
unisgn <unisgn@gmail.com>
2013-10-16 12:46:55 +0800

    m

--- src/header/nrf24l01.h
+++ src/header/nrf24l01.h
@@ -10,10 +10,14 @@ sbit IRQ    = P1^3;
 #define TX_PLOAD_SIZE 4
 #define RX_PLOAD_SIZE 4
 
+extern uchar tx_addr[TX_ADDR_SIZE];
+extern uchar rx_addr[RX_ADDR_SIZE];
+extern uchar tx_buf[TX_PLOAD_SIZE];
+extern uchar rx_buf[RX_PLOAD_SIZE];
 
-void nrf24l01_init(void);
-void nrf24l01_set_ptx_mode(void);
-void nrf24l01_set_prx_mode(void);
-void nrf24l01_tx_packet();
-void nrf24l01_rx_packet();
+extern void nrf24l01_init(void);
+extern void nrf24l01_set_ptx_mode(void);
+extern void nrf24l01_set_prx_mode(void);
+extern void nrf24l01_tx_packet();
+extern void nrf24l01_rx_packet();
 #endif
