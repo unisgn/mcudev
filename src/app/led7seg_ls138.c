d5cb5adb72bcdc4fa600532bc1f76b50215f2ea8
unisgn <unisgn@users.noreply.github.com>
2013-10-18 02:36:17 +0800

    Update led7seg_ls138.c

--- src/app/led7seg_ls138.c
+++ src/app/led7seg_ls138.c
@@ -8,8 +8,7 @@
 
 static void display_digit(uint8_t digit_index, uint8_t digit)
 {
-	
-	if(digit < NUM_SYS_RADIX) {
+    if(digit < NUM_SYS_RADIX) {
         ls138_encode(LED_ARRAY_QTY - 1 - digit_index);
         LED7SEG_DISP(digit);
         sys_delay_5us(10);
@@ -18,7 +17,7 @@ static void display_digit(uint8_t digit_index, uint8_t digit)
 
 static uint32_t power(uint8_t number, uint8_t pow)
 {
-	return (uint32_t) (!pow) ? 1 : (uint32_t) number * power(number, pow - 1);
+    return (uint32_t) (!pow) ? 1 : (uint32_t) number * power(number, pow - 1);
 }
 
 static uint8_t get_digit(uint32_t number, uint8_t digit_index)
