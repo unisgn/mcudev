5ba078fa21466ad3842d85b34cf4fd6146558d28
unisgn <unisgn@users.noreply.github.com>
2013-10-26 09:06:28 +0800

    Update mcu.h

--- src/header/mcu.h
+++ src/header/mcu.h
@@ -1,10 +1,9 @@
-
 #ifndef __MCU_H__
 #define __MCU_H__
 #include <reg51.h>
 #include <types.h>
 
-void sys_init(void);
+void uart_init(void);
 void sys_delay_5us(uint8_t n);
 void sys_delay_1ms(uint8_t n);
 void sys_delay_1s(uint8_t n);
